17760d8fc8a773ad89b57038c7b7e11003ba48b2
Frieder Reinhold <reinhold@trigon-media.com>
2025-04-30 10:56:33 +0200

    Cleanup

--- .config/sway/scripts/taskoverlay.sh
+++ .config/sway/scripts/taskoverlay.sh
@@ -3,7 +3,7 @@ swaymsg "workspace 3"
 swaymsg "exec foot -T Performance -a \"TO_BTop\" zsh -c \"btop\"" &
 sleep 0.3s
 swaymsg "split horizontal"
-swaymsg "exec foot -T Timetracking -a \"TO_Timew\" zsh -c \"while true; do clear; /opt/exxeta/timetracking/sumtoday.py; sleep 5;  done\"" &
+swaymsg "exec foot -T Timetracking -a \"TO_Timew\" zsh -c \"while true; do clear; timetracking_sum_today; sleep 5;  done\"" &
 sleep 0.3s
 swaymsg "split vertical"
 swaymsg "exec foot -T Tasks -a \"TO_Task\" zsh -c \"while true; do clear; obsidian_tasks; sleep 20;  done\"" &
@@ -13,7 +13,7 @@ swaymsg "exec foot -T Command -a \"TO_Cmd\"" &
 sleep 0.3s
 swaymsg "[app_id=TO_Timew] focus"
 swaymsg "split horizontal"
-swaymsg "exec foot -T TimetrackingSum -a \"TO_TimewS\" zsh -c \"while true; do clear; /opt/exxeta/timetracking/aggregate_timew.py --from_date today; sleep 5; done\"" &
+swaymsg "exec foot -T TimetrackingSum -a \"TO_TimewS\" zsh -c \"while true; do clear; timetracking_aggregate --from_date today; sleep 5; done\"" &
 sleep 0.3
 swaymsg "[app_id=TO_Task] focus"
 swaymsg "split horizontal"

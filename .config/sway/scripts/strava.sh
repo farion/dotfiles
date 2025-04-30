b1efb7acd22c0cd5625ee85b1b0690aa2df7f337
Frieder Reinhold <reinhold@trigon-media.com>
2025-04-30 10:00:01 +0200

    Updated Strave widget

--- .config/sway/scripts/strava.sh
+++ .config/sway/scripts/strava.sh
@@ -3,6 +3,7 @@
 STRAVA_CLIENT_ID=$(secret-tool lookup strava strava_client_id)
 STRAVA_CLIENT_SECRET=$(secret-tool lookup strava strava_client_secret)
 STRAVA_REFRESH_TOKEN=$(secret-tool lookup strava strava_refresh_token)
+STRAVA_ATHLETE_ID=$(secret-tool lookup strava strava_athlete_id)
 
 export ACCESS_TOKEN=$(curl -s -X POST https://www.strava.com/api/v3/oauth/token \
 	-d client_id=${STRAVA_CLIENT_ID} \
@@ -10,6 +11,6 @@ export ACCESS_TOKEN=$(curl -s -X POST https://www.strava.com/api/v3/oauth/token 
 	-d grant_type=refresh_token \
 	-d refresh_token=${STRAVA_REFRESH_TOKEN} | jq -r '.access_token' | xargs)
 
-DISTANCE=$(curl -s -X GET https://www.strava.com/api/v3/athletes/43688844/stats -H "Authorization: Bearer ${ACCESS_TOKEN}" | jq -r ".ytd_ride_totals | .distance")
+DISTANCE=$(curl -s -X GET https://www.strava.com/api/v3/athletes/${STRAVA_ATHLETE_ID}/stats -H "Authorization: Bearer ${ACCESS_TOKEN}" | jq -r ".ytd_ride_totals | .distance")
 
 echo $DISTANCE | awk '{print int($1/1000)}'
